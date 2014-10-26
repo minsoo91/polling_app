3f1358f5d8ae00055a020992a7386c4261ff91e1
Minsoo <minsoo91@gmail.com>
2014-10-25 17:36:44 -0700

    Add belongs_to and has_many associations

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,17 @@
 class Response < ActiveRecord::Base
 	validates :user_id, :answer_choice_id, presence: true
+
+	belongs_to(
+		:user,
+		class_name: "User",
+		foreign_key: :user_id,
+		primary_key: :id
+	)
+
+	belongs_to(
+		:answer_choice,
+		class_name: "AnswerChoice",
+		foreign_key: :answer_choice_id,
+		primary_key: :id
+	)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
 	validates :user_name, presence: true, uniqueness: true
+
+	has_many(
+		:polls,
+		class_name: "Poll",
+		foreign_key: :author_id,
+		primary_key: :id
+	)
+
+	has_many(
+		:responses,
+		class_name: "Response",
+		foreign_key: :user_id,
+		primary_key: :id
+	)
 end
